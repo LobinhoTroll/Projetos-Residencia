3f49f9e1cbead53837102df49764bfc96b31e043
Gabriel <gabrielteles931@gmail.com>
2023-08-24 09:18:50 -0300

    Initial commit

---
+++ meuprojeto1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+
+char nome;
+
+cout << "Digite seu nome: ";
+cin >> nome;
+cout << "Bom dia" << nome;
+
+return 0;
+}
