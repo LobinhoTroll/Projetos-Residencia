3ea5a6e09734d59d3135f97c1fdb2d78def7480f
Gabriel <gabrielteles931@gmail.com>
2023-08-25 08:38:54 -0300

    Adicionei a questão 3

---
+++ Projeto2/meuprog2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void){
+
+int a,b;
+
+cout << "Digite dois numeros inteiros:" << endl;
+cin >> a;
+cout << "e" << endl;
+cin >> b;
+cout << "Aqui está a lista com todas as operações matematicas utilizando os numeros fornecidos:" << endl;
+cout << "Soma: " << a + b << endl;
+cout << "Subtração: " << a - b << endl;
+cout << "Multiplicação: " << a + b << endl;
+cout << "Divisão: " << a / b << endl;
+cout << "Resto: " << a % b << endl;
+return 0;
+}
